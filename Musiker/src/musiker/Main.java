adc815d9a0a0378f24b96610cef4406929dbe917
Sinsnickers <63021634+Sinsnickers@users.noreply.github.com>
2022-06-12 13:30:09 +0200

    Aufgabe 4 b und c beantwortet

--- Musiker/src/musiker/Main.java
+++ Musiker/src/musiker/Main.java
@@ -14,9 +14,12 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Band metallica = new Band("2002");
+        Band metallica = new Band("Metallic","2002");
         Musiker james_hetfield = new Musiker("James Hetfield", "James Alan Hetfield");
-        metallica.fuegeMusikerHinzu(james_hetfield);
     }
     
 }
+/*Im großen und ganzen stimme ich mit dem Lösungsvorschlag des Praktikanten zu. Jedoch würde ich anstelle der Komopostion eine Aggregation einsetzen. 
+Natürlich gibt es Musiker, die nur in einer Band spielen und alleine nicht erfolgreich wären. Jedoch macht jeder Musiker für sich alleine auch Musik,
+somit empfinde ich die Aggregation als "bessere" Lösung
+*/
